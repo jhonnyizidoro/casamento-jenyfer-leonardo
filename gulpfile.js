55051e1a939537a1774060180b2e9991f8b13357
Jhonny Izidoro <jhonny.menarim@hotmail.com>
2020-03-02 02:52:33 -0300

    Header / Nav desktop

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ task(sass = () => {
 		}).on('error', error => {
 			let errorFile = error.file.split('/')
 			errorFile = errorFile[errorFile.length - 1]
-			console.log("\x1b[41m", "\x1b[30m", `Erro na linha ${error.line} do arquivo ${errorFile} na loja ${projectDir}.\nErro: ${error.messageOriginal}`, "\x1b[0m")
+			console.log("\x1b[41m", "\x1b[30m", `Erro na linha ${error.line} do arquivo ${errorFile}.\nErro: ${error.messageOriginal}`, "\x1b[0m")
 		}),
 		dest('public/css'),
 		browserSync.stream()
