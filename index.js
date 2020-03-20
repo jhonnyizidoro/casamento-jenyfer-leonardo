e061f382c2bad1a2368597240dad3a83473171db
Jhonny Izidoro <jhonny.menarim@hotmail.com>
2020-03-20 03:44:08 -0300

    app > index

--- index.js
+++ index.js
@@ -9,32 +9,32 @@ const assets = require('express-asset-versions'),
 	bodyParser = require('body-parser')
 
 //Init express
-const app = express()
+const index = express()
 
 //Global scope
 Object.assign(global, env)
 Object.assign(global, helpers)
 
 //App config
-app.set('port', process.env.PORT || 80)
-app.set('views', `${__dirname}/views`)
-app.set('view engine', 'pug')
-app.enable('trust proxy')
+index.set('port', process.env.PORT || 80)
+index.set('views', `${__dirname}/views`)
+index.set('view engine', 'pug')
+index.enable('trust proxy')
 
 //Middleware
-app.use(compression())
-app.use(bodyParser.json())
-app.use(express.urlencoded({ extended: false }))
-app.use('/', express.static(`${__dirname}/public`))
-app.use('/', express.static(`${__dirname}/public/global`))
-app.use(assets('/', `${__dirname}/public`))
-app.use(fileUpload({}))
-app.use(session({
+index.use(compression())
+index.use(bodyParser.json())
+index.use(express.urlencoded({ extended: false }))
+index.use('/', express.static(`${__dirname}/public`))
+index.use('/', express.static(`${__dirname}/public/global`))
+index.use(assets('/', `${__dirname}/public`))
+index.use(fileUpload({}))
+index.use(session({
 	saveUninitialized: true,
 	secret: 'ecNewNodeJS',
 	resave: true,
 }))
-app.use((req, res, next) => {
+index.use((req, res, next) => {
 	global.host = `${req.protocol}://${req.get('host')}`
 	global.url = `${global.host}${req.url}`
 	global.path = req.url
@@ -43,14 +43,14 @@ app.use((req, res, next) => {
 })
 
 //Router
-routes(app)
+routes(index)
 
 // error handler
-app.use((err, req, res, next) => {
+index.use((err, req, res, next) => {
 	console.log(err)
 	res.status(err.status || 500)
 	res.render('error')
 })
 
 // Init Server
-app.listen(process.env.PORT || 80, () => console.log('server On!'))
+index.listen(process.env.PORT || 80, () => console.log('server On!'))
